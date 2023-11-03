fd4b6dc74332529e0b88820aa0e224b037462bf3
Alex <thindie1@mail.ru>
2023-11-04 01:16:40 +0200

    add network component

--- cryptoview/src/main/java/com/example/cryptoview/application/di/AppComponent.kt
+++ cryptoview/src/main/java/com/example/cryptoview/application/di/AppComponent.kt
@@ -1,22 +1,28 @@
 package com.example.cryptoview.application.di
 
 import android.content.Context
+import com.example.core.network.di.NetworkComponent
+import com.example.core.network.di.NetworkProvider
 import dagger.BindsInstance
 import dagger.Component
 
-@Component
-interface AppComponent {
+@Component(dependencies = [NetworkProvider::class])
+interface AppComponent : DependenciesProvider {
 
     companion object {
         fun init(context: Context): AppComponent {
+            val networkProvider: NetworkProvider = NetworkComponent.init()
             return DaggerAppComponent
                 .factory()
-                .create(context)
+                .create(context, networkProvider)
         }
     }
 
     @Component.Factory
     interface Factory {
-        fun create(@BindsInstance context: Context): AppComponent
+        fun create(
+            @BindsInstance context: Context,
+            networkProvider: NetworkProvider,
+        ): AppComponent
     }
 }
---
+++ cryptoview/src/main/java/com/example/cryptoview/application/di/DependenciesProvider.kt
@@ -0,0 +1,5 @@
+package com.example.cryptoview.application.di
+
+import com.example.core.network.di.NetworkProvider
+
+interface DependenciesProvider : NetworkProvider
