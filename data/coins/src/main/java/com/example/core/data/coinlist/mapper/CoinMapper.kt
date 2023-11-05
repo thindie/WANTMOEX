1c5bf35e970b842acb478a2405935fb9a28016b9
Alex <thindie1@mail.ru>
2023-11-05 20:10:23 +0200

    data: coin: add dependencies, add coin Mapper, add repo impl

---
+++ data/coins/src/main/java/com/example/core/data/coinlist/mapper/CoinMapper.kt
@@ -0,0 +1,47 @@
+package com.example.core.data.coinlist.mapper
+
+import com.example.core.network.dto.CoinDto
+import com.example.thindie.domain.Coin
+
+fun CoinDto.coinDtoToCoin() =
+    Coin(
+        type = type,
+        market = market,
+        fromSymbol = fromSymbol,
+        toSymbol = toSymbol,
+        flags = flags,
+        price = price,
+        lastUpdate = lastUpdate,
+        lastVolume = lastVolume,
+        lastVolumeTo = lastVolumeTo,
+        lastTradeId = lastTradeId,
+        volumeDay = volumeDay,
+        volumeDayTo = volumeDayTo,
+        volume24Hour = volume24Hour,
+        volume24HourTo = volume24HourTo,
+        openDay = openDay,
+        highDay = highDay,
+        lowDay = lowDay,
+        open24Hour = open24Hour,
+        high24Hour = high24Hour,
+        low24Hour = low24Hour,
+        lastMarket = lastMarket,
+        volumeHour = volumeHour,
+        volumeHourTo = volumeHourTo,
+        openHour = openHour,
+        highHour = highHour,
+        lowHour = lowHour,
+        topTierVolume24Hour = topTierVolume24Hour,
+        topTierVolume24HourTo = topTierVolume24HourTo,
+        change24Hour = change24Hour,
+        changePCT24Hour = changePCT24Hour,
+        changeDay = changeDay,
+        changePCTDay = changePCTDay,
+        supply = supply,
+        mktCap = mktCap,
+        totalVolume24Hour = totalVolume24Hour,
+        totalVolume24HourTo = totalVolume24HourTo,
+        totalTopTierVolume24Hour = totalTopTierVolume24Hour,
+        totalTopTierVolume24HourTo = totalTopTierVolume24HourTo,
+        imageUrl = imageUrl
+    )
