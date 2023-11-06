b7109c0946a240d06eac24505b4776f021f3df2c
Alex <thindie1@mail.ru>
2023-11-06 17:05:35 +0200

    di: add viewmodel logic for injection. (factory)

---
+++ cryptoview/src/main/java/com/example/cryptoview/application/viewmodelfactory/ViewModelKey.kt
@@ -0,0 +1,10 @@
+package com.example.cryptoview.application.viewmodelfactory
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@MapKey
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
