3c6b25ee3c1f8b789fc217467340b3435fe783a7
Alex <thindie1@mail.ru>
2023-11-06 23:13:40 +0200

    fix unscoped SettingsComponent,

--- data/settings/src/main/java/com/example/core/data/uistatesettings/di/SettingsComponent.kt
+++ data/settings/src/main/java/com/example/core/data/uistatesettings/di/SettingsComponent.kt
@@ -1,7 +1,9 @@
 package com.example.core.data.uistatesettings.di
 
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [SettingsRepositoryModule::class])
 interface SettingsComponent : SettingsProvider {
     companion object {
