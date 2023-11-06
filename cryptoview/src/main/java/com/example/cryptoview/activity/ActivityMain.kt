1fcb0dd643608d86871ed17dc469c65b7b84d29d
Alex <thindie1@mail.ru>
2023-11-06 17:09:48 +0200

    main: inject factory; add main Viewmodel

--- cryptoview/src/main/java/com/example/cryptoview/activity/ActivityMain.kt
+++ cryptoview/src/main/java/com/example/cryptoview/activity/ActivityMain.kt
@@ -2,27 +2,22 @@ package com.example.cryptoview.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
-import com.example.core.network.apiservice.ApiService
 import com.example.cryptoview.application.di.AppComponent
+import com.example.cryptoview.application.viewmodelfactory.ViewModelFactory
 import javax.inject.Inject
-import kotlinx.coroutines.launch
 
 class ActivityMain : ComponentActivity() {
 
     @Inject
-    lateinit var service: ApiService
+    lateinit var factory: ViewModelFactory
+    private val mainViewModel: MainViewModel by viewModels { factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         initDaggerComponent()
-
-        lifecycleScope.launch {
-            val result = service.getTopCoinsEURO(limit = 10).getPopulated()
-            val result1 = service.getTopCoinsRUB(limit = 10).getPopulated()
-        }
     }
 
     private fun initDaggerComponent() {
