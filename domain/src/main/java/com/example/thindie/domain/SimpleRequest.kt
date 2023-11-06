3ed79e5eca8c9d1575b409e3b9c8f5bc9257d6d4
Alex <thindie1@mail.ru>
2023-11-06 22:40:39 +0200

    domain: refactor SimpleRequest to generic

--- domain/src/main/java/com/example/thindie/domain/SimpleRequest.kt
+++ domain/src/main/java/com/example/thindie/domain/SimpleRequest.kt
@@ -3,5 +3,5 @@ package com.example.thindie.domain
 sealed class SimpleRequest {
     object Refresh : SimpleRequest()
 
-    data class Set(val order: Int) : SimpleRequest()
+    data class Set<T : Any>(val t: T) : SimpleRequest()
 }
