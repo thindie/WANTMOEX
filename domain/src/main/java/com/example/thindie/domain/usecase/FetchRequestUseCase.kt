54fc34a6436fbae653192204baed3c5d0e358b79
Alex <thindie1@mail.ru>
2023-11-06 22:45:57 +0200

    fix dependencies ordering

--- domain/src/main/java/com/example/thindie/domain/usecase/FetchRequestUseCase.kt
+++ domain/src/main/java/com/example/thindie/domain/usecase/FetchRequestUseCase.kt
@@ -1,8 +1,8 @@
 package com.example.thindie.domain.usecase
 
-import com.example.thindie.domain.Coin
 import com.example.thindie.domain.Repository
 import com.example.thindie.domain.SimpleRequest
+import com.example.thindie.domain.entities.Coin
 import javax.inject.Inject
 
 class FetchRequestUseCase @Inject constructor(private val repository: Repository<List<Coin>>) {
